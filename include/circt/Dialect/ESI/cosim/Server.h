thomasnormal/circt

commit e0a86025e1dac1d0407c60efc544d2e203ae69f2

Remove `using namespace mlir` from header files
This change aims to remove all instances of `using namespace mlir` from
our header files.  Many types have been manually imported into the
`circt` namespace in `circt/Support/LLVM.h`.  These types include all
core MLIR functionality types (IR, Diagnostic), utility types, rewrite
patterns and conversions, builtin IR types and attrs, and the module op.

Not imported were any operations (other than ModuleOp), matchers (e.g.
m_Zero), anything in the `impl` namespace, and interfaces.  There are
some problems with interface code generation from ODS, so some
interfaces were imported.

Some further cleanup would be useful to remove the `mlir::` namespace
qualifier where we no longer need it.

lib/Dialect/ESI/ESITranslations.cpp
@@ -22,7 +22,6 @@
 #include "circt/Dialect/ESI/CosimSchema.h"
 #endif
 
-using namespace mlir;
 using namespace circt::esi;
 
 //===----------------------------------------------------------------------===//
@@ -167,8 +166,9 @@ static LogicalResult exportCosimSchema(ModuleOp module, llvm::raw_ostream &os) {
 
 void circt::esi::registerESITranslations() {
 #ifdef CAPNP
-  TranslateFromMLIRRegistration cosimToCapnp(
-      "export-esi-capnp", exportCosimSchema, [](DialectRegistry &registry) {
+  mlir::TranslateFromMLIRRegistration cosimToCapnp(
+      "export-esi-capnp", exportCosimSchema,
+      [](mlir::DialectRegistry &registry) {
         registry
             .insert<ESIDialect, circt::rtl::RTLDialect, circt::sv::SVDialect,
                     mlir::StandardOpsDialect, mlir::BuiltinDialect>();

include/circt/Conversion/FIRRTLToLLHD/FIRRTLToLLHD.h
@@ -23,7 +23,6 @@ class OperationPass;
 } // namespace mlir
 
 namespace circt {
-using namespace mlir;
 std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
 createConvertFIRRTLToLLHDPass();
 } // namespace circt

include/circt/Conversion/LLHDToLLVM/LLHDToLLVM.h
@@ -14,21 +14,17 @@
 #ifndef CIRCT_CONVERSION_LLHDTOLLVM_LLHDTOLLVM_H
 #define CIRCT_CONVERSION_LLHDTOLLVM_LLHDTOLLVM_H
 
+#include "circt/Support/LLVM.h"
 #include <memory>
 
 namespace mlir {
-class ModuleOp;
 class LLVMTypeConverter;
-class OwningRewritePatternList;
-template <typename T>
-class OperationPass;
 } // namespace mlir
 
 namespace circt {
-using namespace mlir;
 
 /// Get the LLHD to LLVM conversion patterns.
-void populateLLHDToLLVMConversionPatterns(LLVMTypeConverter &converter,
+void populateLLHDToLLVMConversionPatterns(mlir::LLVMTypeConverter &converter,
                                           OwningRewritePatternList &patterns,
                                           size_t &sigCounter,
                                           size_t &regCounter);

include/circt/Dialect/FIRRTL/FIRRTLDialect.h
@@ -13,12 +13,13 @@
 #ifndef CIRCT_DIALECT_FIRRTL_DIALECT_H
 #define CIRCT_DIALECT_FIRRTL_DIALECT_H
 
+#include "circt/Support/LLVM.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/Dialect.h"
 
 namespace circt {
 namespace firrtl {
-using namespace mlir;
+
 class FIRRTLType;
 
 class FIRRTLDialect : public Dialect {

include/circt/Dialect/FIRRTL/FIRRTLTypes.h
@@ -14,6 +14,7 @@
 #define CIRCT_DIALECT_FIRRTL_TYPES_H
 
 #include "circt/Dialect/FIRRTL/FIRRTLDialect.h"
+#include "circt/Support/LLVM.h"
 #include "mlir/IR/Types.h"
 
 namespace circt {
@@ -24,8 +25,6 @@ struct FlipTypeStorage;
 struct BundleTypeStorage;
 struct VectorTypeStorage;
 } // namespace detail.
-
-using namespace mlir;
 
 class ClockType;
 class ResetType;

include/circt/Dialect/Handshake/HandshakeOps.h
@@ -13,6 +13,7 @@
 #ifndef CIRCT_HANDSHAKEOPS_OPS_H_
 #define CIRCT_HANDSHAKEOPS_OPS_H_
 
+#include "circt/Support/LLVM.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -31,7 +32,6 @@
 namespace circt {
 namespace handshake {
 
-using namespace mlir;
 class TerminatorOp;
 
 class HandshakeOpsDialect : public Dialect {

include/circt/Dialect/Handshake/HandshakeOps.td
@@ -141,18 +141,18 @@ def FuncOp : Op<Handshake_Dialect, "func", [
   }];
   let printer = [{
     FunctionType fnType = getType();
-    impl::printFunctionLikeOp(p, *this, fnType.getInputs(), /*isVariadic=*/true,
-                              fnType.getResults());
+    mlir::impl::printFunctionLikeOp(p, *this, fnType.getInputs(),
+        /*isVariadic=*/true, fnType.getResults());
   }];
   let parser = [{
     auto buildFuncType =
         [](Builder & builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
-           impl::VariadicFlag, std::string &) {
+           mlir::impl::VariadicFlag, std::string &) {
       return builder.getFunctionType(argTypes, results);
     };
 
-    return impl::parseFunctionLikeOp(parser, result, /*allowVariadic=*/true,
-                                     buildFuncType);
+    return mlir::impl::parseFunctionLikeOp(parser, result,
+        /*allowVariadic=*/true, buildFuncType);
   }];
 }
 

include/circt/Dialect/LLHD/IR/LLHD.td
@@ -102,11 +102,11 @@ class LLHD_ArithmeticOrBitwiseOp<string mnemonic, list<OpTrait> traits = []>
   let results = (outs AnySignlessInteger);
 
   let parser = [{
-      return impl::parseOneResultSameOperandTypeOp(parser, result);
+      return mlir::impl::parseOneResultSameOperandTypeOp(parser, result);
   }];
 
   let printer = [{
-      impl::printOneResultOp(this->getOperation(), p);
+      mlir::impl::printOneResultOp(this->getOperation(), p);
   }];
 }
 

commit c3789f32b5fb5c30869630296ad0d1b60e2ff347

[ESI] Fix integration test issues
- Don't forward declare thread.  Does not work on OSX, and in general
  you can't forward declare classes from the `std` namespace.
- Fix namespace issues caused by e0a86025e1.
- Fix cosim test: fix constants

include/circt/Dialect/ESI/cosim/Server.h
@@ -17,10 +17,7 @@
 #define CIRCT_DIALECT_ESI_COSIM_SERVER_H
 
 #include "circt/Dialect/ESI/cosim/Endpoint.h"
-
-namespace std {
-class thread;
-} // namespace std
+#include <thread>
 
 namespace circt {
 namespace esi {

lib/Dialect/ESI/ESITranslations.cpp
@@ -4,12 +4,12 @@
 // - Cap'nProto schema generation
 //
 //===----------------------------------------------------------------------===//
+
 #include "circt/Dialect/ESI/ESIDialect.h"
 #include "circt/Dialect/ESI/ESIOps.h"
-
 #include "circt/Dialect/RTL/RTLDialect.h"
 #include "circt/Dialect/SV/SVDialect.h"
-
+#include "circt/Support/LLVM.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/BuiltinDialect.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -22,6 +22,7 @@
 #include "circt/Dialect/ESI/CosimSchema.h"
 #endif
 
+using namespace circt;
 using namespace circt::esi;
 
 //===----------------------------------------------------------------------===//
@@ -40,7 +41,7 @@ struct ExportCosimSchema {
       : module(module), os(os), diag(module.getContext()->getDiagEngine()),
         unknown(UnknownLoc::get(module.getContext())) {
     diag.registerHandler([this](Diagnostic &diag) -> LogicalResult {
-      if (diag.getSeverity() == DiagnosticSeverity::Error)
+      if (diag.getSeverity() == mlir::DiagnosticSeverity::Error)
         ++errorCount;
       return failure();
     });
@@ -61,7 +62,7 @@ struct ExportCosimSchema {
 private:
   ModuleOp module;
   llvm::raw_ostream &os;
-  DiagnosticEngine &diag;
+  mlir::DiagnosticEngine &diag;
   const Location unknown;
   size_t errorCount = 0;
 

test/Dialect/ESI/cosim.mlir
@@ -96,9 +96,9 @@ rtl.module @top(%clk:i1, %rstn:i1) -> () {
   // SV:      assert(rootPointer_2[6'h0+:32] == 32'h0);
   // SV:      assert(rootPointer_2[6'h20+:16] == 16'h0);
   // SV:      assert(rootPointer_2[6'h30+:16] == 16'h1);
-  // SV:      assert(i_ptr[6'h0+:2] == 2'h0);
-  // SV:      assert(i_ptr[6'h1F+:3] == 3'h5);
-  // SV:      assert($signed(i_ptr[6'h22+:29]) <= 29'sh0);
+  // SV:      assert(i_ptr[6'h0+:2] == 2'h1);
+  // SV:      assert(i_ptr[6'h20+:3] == 3'h5);
+  // SV:      assert(i_ptr[6'h23+:29] <= 29'h4);
   // SV:    end
   // SV:  end // always @(posedge)
   // SV:  wire [63:0] rootPointer_2 = ArrTestEP_DataOut[9'h0+:64];