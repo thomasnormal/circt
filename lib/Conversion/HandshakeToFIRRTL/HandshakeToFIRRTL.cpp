thomasnormal/circt

commit ffbaf137ba91cbbc45860d0a800c0c7a428ee838

[FIRRTL] rename FModuleOp::getBodyBlock() to getBody() (#1940)
This change renames the `getBodyBlock()` function to `getBody()`, which matches
the CircuitOp API.  In a module there is only ever one region with one block in
it, and there is rarely a reason to get the Region. Naming this wasn't possible
before as `FunctionLike` was squatting on the `getBody()` name, which has
recently been dropped.

lib/Conversion/HandshakeToFIRRTL/HandshakeToFIRRTL.cpp
@@ -37,7 +37,7 @@ static void legalizeFModule(FModuleOp moduleOp) {
   SmallVector<Operation *, 8> connectOps;
   moduleOp.walk([&](ConnectOp op) { connectOps.push_back(op); });
   for (auto op : connectOps)
-    op->moveBefore(&moduleOp.getBodyBlock()->back());
+    op->moveBefore(&moduleOp.getBody()->back());
 }
 
 /// Get the corresponding FIRRTL type given the built-in data type. Current
@@ -494,27 +494,11 @@ static FModuleOp createTopModuleOp(handshake::FuncOp funcOp, unsigned numClocks,
   // Create a FIRRTL module, and inline the funcOp into it.
   auto topModuleOp = rewriter.create<FModuleOp>(
       funcOp.getLoc(), rewriter.getStringAttr(funcOp.getName()), ports);
-  rewriter.inlineRegionBefore(funcOp.getBody(), topModuleOp.getBody(),
-                              topModuleOp.end());
 
-  // Merge the second block (inlined from funcOp) of the top-module into the
-  // entry block.
-  auto blockIterator = topModuleOp.getBody().begin();
-  Block *entryBlock = &*blockIterator;
-  Block *secondBlock = &*(++blockIterator);
-
-  // Replace uses of each argument of the second block with the corresponding
-  // argument of the entry block.
-  argIndex = 0;
-  for (auto &oldArg : secondBlock->getArguments()) {
-    oldArg.replaceAllUsesWith(entryBlock->getArgument(argIndex));
-    ++argIndex;
-  }
-
-  // Move all operations of the second block to the entry block.
-  entryBlock->getOperations().splice(entryBlock->end(),
-                                     secondBlock->getOperations());
-  rewriter.eraseBlock(secondBlock);
+  auto replacementArgs =
+      topModuleOp.getArguments().take_front(funcOp.getNumArguments());
+  rewriter.mergeBlocks(&funcOp.getBody().front(), topModuleOp.getBody(),
+                       replacementArgs);
 
   return topModuleOp;
 }
@@ -2142,7 +2126,7 @@ static void createInstOp(Operation *oldOp, FModuleOp subModuleOp,
     unsigned numIns = oldOp->getNumOperands();
     unsigned numArgs = numIns + oldOp->getNumResults();
 
-    auto topArgs = topModuleOp.getBody().front().getArguments();
+    auto topArgs = topModuleOp.getBody()->getArguments();
     auto firstClock = std::find_if(topArgs.begin(), topArgs.end(),
                                    [](BlockArgument &arg) -> bool {
                                      return arg.getType().isa<ClockType>();
@@ -2224,7 +2208,7 @@ struct HandshakeFuncOpLowering : public OpConversionPattern<handshake::FuncOp> {
     auto topModuleOp = createTopModuleOp(funcOp, /*numClocks=*/1, rewriter);
 
     // Traverse and convert each operation in funcOp.
-    for (Operation &op : topModuleOp.getBody().front()) {
+    for (Operation &op : *topModuleOp.getBody()) {
       if (isa<handshake::ReturnOp>(op))
         convertReturnOp(&op, topModuleOp, funcOp, rewriter);
 
@@ -2239,8 +2223,8 @@ struct HandshakeFuncOpLowering : public OpConversionPattern<handshake::FuncOp> {
           subModuleOp = createSubModuleOp(topModuleOp, &op, hasClock, rewriter);
 
           Location insertLoc = subModuleOp.getLoc();
-          auto &bodyBlock = subModuleOp.getBody().front();
-          rewriter.setInsertionPoint(&bodyBlock, bodyBlock.end());
+          auto *bodyBlock = subModuleOp.getBody();
+          rewriter.setInsertionPoint(bodyBlock, bodyBlock->end());
 
           ValueVectorList portList =
               extractSubfields(subModuleOp, insertLoc, rewriter);

include/circt/Dialect/FIRRTL/FIRRTLStructure.td
@@ -73,17 +73,14 @@ def FModuleOp : FIRRTLOp<"module", [IsolatedFromAbove, Symbol, SingleBlock,
   ];
 
   let extraClassDeclaration = [{
-    Region &getBody() { return getRegion(); }
-    Block *getBodyBlock() { return &body().front(); }
+    Block *getBody() { return &body().front(); }
 
-    using iterator = Region::BlockListType::iterator;
-    iterator begin() { return getBody().begin(); }
-    iterator end() { return getBody().end(); }
-
-    Block &front() { return getBody().front(); }
+    using iterator = Block::iterator;
+    iterator begin() { return getBody()->begin(); }
+    iterator end() { return getBody()->end(); }
 
     Block::BlockArgListType getArguments() {
-      return getBody().getArguments();
+      return getBody()->getArguments();
     }
 
     // Return the block argument for the port with the specified index.

lib/Conversion/FIRRTLToHW/LowerToHW.cpp
@@ -330,7 +330,7 @@ static FirMemory analyzeMemOp(MemOp op) {
 
 static SmallVector<FirMemory> collectFIRRTLMemories(FModuleOp module) {
   SmallVector<FirMemory> retval;
-  for (auto op : module.getBody().getOps<MemOp>())
+  for (auto op : module.getBody()->getOps<MemOp>())
     retval.push_back(analyzeMemOp(op));
   return retval;
 }
@@ -1125,7 +1125,7 @@ void FIRRTLModuleLowering::lowerModuleBody(
   outputOp->setOperands(outputs);
 
   // Finally splice the body over, don't move the old terminator over though.
-  auto &oldBlockInstList = oldModule.getBodyBlock()->getOperations();
+  auto &oldBlockInstList = oldModule.getBody()->getOperations();
   auto &newBlockInstList = newModule.getBodyBlock()->getOperations();
   newBlockInstList.splice(Block::iterator(cursor), oldBlockInstList,
                           oldBlockInstList.begin(), oldBlockInstList.end());
@@ -1387,14 +1387,14 @@ void FIRRTLLowering::run() {
   // FIRRTL FModule is a single block because FIRRTL ops are a DAG.  Walk
   // through each operation, lowering each in turn if we can, introducing
   // casts if we cannot.
-  auto *body = theModule.getBodyBlock();
+  auto &body = theModule.getBody();
   randomizePrologEmitted = false;
 
   SmallVector<Operation *, 16> opsToRemove;
 
   // Iterate through each operation in the module body, attempting to lower
   // each of them.  We maintain 'builder' for each invocation.
-  for (auto &op : body->getOperations()) {
+  for (auto &op : body.front().getOperations()) {
     builder.setInsertionPoint(&op);
     builder.setLoc(op.getLoc());
     auto done = succeeded(dispatchVisitor(&op));

lib/Dialect/FIRRTL/Export/FIREmitter.cpp
@@ -219,11 +219,11 @@ void Emitter::emitModule(FModuleOp op) {
   // Emit the ports.
   auto ports = op.getPorts();
   emitModulePorts(ports, op.getArguments());
-  if (!ports.empty() && !op.getBodyBlock()->empty())
+  if (!ports.empty() && !op.getBody()->empty())
     os << "\n";
 
   // Emit the module body.
-  emitStatementsInBlock(*op.getBodyBlock());
+  emitStatementsInBlock(*op.getBody());
 
   reduceIndent();
   valueNames.clear();

lib/Dialect/FIRRTL/FIRRTLOps.cpp
@@ -338,7 +338,7 @@ SmallVector<PortInfo> FModuleOp::getPorts() {
   auto portNamesAttr = getPortNames();
   auto portDirections = getPortDirections();
   // FModuleOp has the ports as the BlockArgument's of the first block.
-  auto moduleBlock = getBodyBlock();
+  auto moduleBlock = getBody();
   for (auto portArgAndIndex : llvm::enumerate(moduleBlock->getArguments())) {
     BlockArgument portArg = portArgAndIndex.value();
     size_t portIdx = portArgAndIndex.index();
@@ -373,7 +373,7 @@ SmallVector<PortInfo> FExtModuleOp::getPorts() {
 
 // Return the port with the specified name.
 BlockArgument FModuleOp::getArgument(size_t portNumber) {
-  return getBodyBlock()->getArgument(portNumber);
+  return getBody()->getArgument(portNumber);
 }
 
 /// Inserts the given ports. The insertion indices are expected to be in order.
@@ -385,7 +385,7 @@ void FModuleOp::insertPorts(ArrayRef<std::pair<unsigned, PortInfo>> ports) {
   unsigned oldNumArgs = getNumPorts();
   unsigned newNumArgs = oldNumArgs + ports.size();
 
-  auto *body = getBodyBlock();
+  auto *body = getBody();
 
   // Add direction markers and names for new ports.
   SmallVector<Direction> existingDirections = direction::unpackAttribute(*this);
@@ -456,7 +456,7 @@ void FModuleOp::erasePorts(ArrayRef<unsigned> portIndices) {
   (*this)->setAttr("portTypes", ArrayAttr::get(getContext(), newPortTypes));
 
   // Erase the block arguments.
-  getBodyBlock()->eraseArguments(portIndices);
+  getBody()->eraseArguments(portIndices);
 }
 
 static void buildModule(OpBuilder &builder, OperationState &result,
@@ -677,7 +677,7 @@ static void printFModuleOp(OpAsmPrinter &p, FModuleOp op) {
   // Print the body if this is not an external function. Since this block does
   // not have terminators, printing the terminator actually just prints the last
   // operation.
-  Region &body = op.getBody();
+  Region &body = op.body();
   if (!body.empty())
     p.printRegion(body, /*printEntryBlockArgs=*/false,
                   /*printBlockTerminators=*/true);

lib/Dialect/FIRRTL/Import/FIRParser.cpp
@@ -3492,7 +3492,7 @@ FIRCircuitParser::parseModuleBody(DeferredModuleToParse &deferredModule) {
       return failure();
   }
 
-  FIRStmtParser stmtParser(*moduleOp.getBodyBlock(), moduleContext);
+  FIRStmtParser stmtParser(*moduleOp.getBody(), moduleContext);
 
   // Parse the moduleBlock.
   auto result = stmtParser.parseSimpleStmtBlock(deferredModule.indent);

lib/Dialect/FIRRTL/Transforms/BlackBoxMemory.cpp
@@ -211,7 +211,7 @@ static FModuleOp createWrapperModule(MemOp op,
   moduleOp->moveAfter(extModuleOp);
 
   // Create the module
-  builder.setInsertionPointToStart(moduleOp.getBodyBlock());
+  builder.setInsertionPointToStart(moduleOp.getBody());
   auto instanceOp = createInstance(builder, op.getLoc(), extModuleOp.getName(),
                                    op.nameAttr(), extPorts);
 

lib/Dialect/FIRRTL/Transforms/ExpandWhens.cpp
@@ -510,7 +510,7 @@ bool ModuleVisitor::run(FModuleOp module) {
   }
 
   // Process the body of the module.
-  for (auto &op : llvm::make_early_inc_range(*module.getBodyBlock())) {
+  for (auto &op : llvm::make_early_inc_range(*module.getBody())) {
     dispatchVisitor(&op);
   }
   return anythingChanged;

commit 607eb19754e44573b5fc2376e4c21811a54cb1a1

[Handshake] Do early replacement of argument SSA values (#1968)
ffbaf13 introduced a "hidden" regression given that (as far as i know) `rewriter.mergeBlocks` does not instantly replace block SSA arguments, but instead at some later point in time.

This is visible if we dump the top module at any point after argument conversion:
```mlir
firrtl.module @main(in %arg0: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>, data: uint<8>>, in %arg1: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>, data: uint<64>>, in %arg2: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>, data: uint<64>>, out %arg3: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>, data: uint<8>>, out %arg4: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>>, out %arg5: !firrtl.bundle<valid: uint<1>, ready flip: uint<1>>, in %clock: !firrtl.clock, in %reset: !firrtl.uint<1>) {
  %0:3 = "handshake.memory"(<<UNKNOWN SSA VALUE>>, <<UNKNOWN SSA VALUE>>, <<UNKNOWN SSA VALUE>>) {id = 0 : i32, ld_count = 1 : i32, lsq = false, st_count = 1 : i32, type = memref<10xi8>} : (i8, index, index) -> (i8, none, none)
  handshake.return %0#0, %0#1, %0#2 : i8, none, none
}
```

This is surprisingly not raising any issues to the conversion process since all of the operator logic is outlined into separate modules. However, I need to do a transformation on the input argument of the top-level module, which ends up triggering this issue.

Notably, this is the same issue that we've run into in both StandardToHandshake and SCFToCalyx; placing too many assumptions on rewriter side-effects being comitted after a rewrite call has been done. Over there, we've employed a technique for doing "partial lowering", splitting out distinct lowering steps (which does not erase the top-level op) into separate rewrite pattern application runs, to ensure that rewriter side-effects are committed.

lib/Conversion/HandshakeToFIRRTL/HandshakeToFIRRTL.cpp
@@ -495,11 +495,29 @@ static FModuleOp createTopModuleOp(handshake::FuncOp funcOp, unsigned numClocks,
   auto topModuleOp = rewriter.create<FModuleOp>(
       funcOp.getLoc(), rewriter.getStringAttr(funcOp.getName()), ports);
 
-  auto replacementArgs =
-      topModuleOp.getArguments().take_front(funcOp.getNumArguments());
-  rewriter.mergeBlocks(&funcOp.getBody().front(), topModuleOp.getBody(),
-                       replacementArgs);
+  rewriter.inlineRegionBefore(funcOp.body(), topModuleOp.body(),
+                              topModuleOp.body().end());
 
+  // In the following section, we manually merge the two regions and manually
+  // replace arguments. This is an alternative to using rewriter.mergeBlocks; we
+  // do this to ensure that argument SSA values are replaced instantly, instead
+  // of late, as would be the case for mergeBlocks.
+
+  // Merge the second block (inlined from funcOp) of the top-module into the
+  // entry block.
+  auto &blockIterator = topModuleOp.body().getBlocks();
+  Block *entryBlock = &blockIterator.front();
+  Block *secondBlock = &*std::next(blockIterator.begin());
+
+  // Replace uses of each argument of the second block with the corresponding
+  // argument of the entry block.
+  for (auto &oldArg : enumerate(secondBlock->getArguments()))
+    oldArg.value().replaceAllUsesWith(entryBlock->getArgument(oldArg.index()));
+
+  // Move all operations of the second block to the entry block.
+  entryBlock->getOperations().splice(entryBlock->end(),
+                                     secondBlock->getOperations());
+  rewriter.eraseBlock(secondBlock);
   return topModuleOp;
 }
 