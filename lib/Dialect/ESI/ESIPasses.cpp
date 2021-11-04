thomasnormal/circt

commit daee6494add2c64a6c11a3b7371cb637d3fcd505

Bump LLVM for D111695 (#2074)
Bump LLVM to pull in performance improvements from
https://reviews.llvm.org/D111695.

This required the following fixes:
  - Fix usages of deprecated matchAndRewrite member function that uses
    an ArrayRef<Value>.  Change this to use an OpAdaptor.
  - Use Source Not Argument Materialization in HWToLLHD.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/ESI/ESIPasses.cpp
@@ -285,13 +285,13 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(ChannelBuffer buffer, ArrayRef<Value> operands,
+  matchAndRewrite(ChannelBuffer buffer, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult ChannelBufferLowering::matchAndRewrite(
-    ChannelBuffer buffer, ArrayRef<Value> operands,
+    ChannelBuffer buffer, OpAdaptor adaptor,
     ConversionPatternRewriter &rewriter) const {
   auto loc = buffer.getLoc();
 
@@ -846,7 +846,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(PipelineStage stage, ArrayRef<Value> operands,
+  matchAndRewrite(PipelineStage stage, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 
 private:
@@ -855,7 +855,7 @@ private:
 } // anonymous namespace
 
 LogicalResult PipelineStageLowering::matchAndRewrite(
-    PipelineStage stage, ArrayRef<Value> stageOperands,
+    PipelineStage stage, OpAdaptor adaptor,
     ConversionPatternRewriter &rewriter) const {
   auto loc = stage.getLoc();
   auto chPort = stage.input().getType().dyn_cast<ChannelPort>();
@@ -912,13 +912,13 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(NullSourceOp nullop, ArrayRef<Value> operands,
+  matchAndRewrite(NullSourceOp nullop, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult NullSourceOpLowering::matchAndRewrite(
-    NullSourceOp nullop, ArrayRef<Value> stageOperands,
+    NullSourceOp nullop, OpAdaptor adaptor,
     ConversionPatternRewriter &rewriter) const {
   auto innerType = nullop.out().getType().cast<ChannelPort>().getInner();
   Location loc = nullop.getLoc();
@@ -1000,15 +1000,15 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(WrapSVInterface wrap, ArrayRef<Value> operands,
+  matchAndRewrite(WrapSVInterface wrap, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult
-WrapInterfaceLower::matchAndRewrite(WrapSVInterface wrap,
-                                    ArrayRef<Value> operands,
+WrapInterfaceLower::matchAndRewrite(WrapSVInterface wrap, OpAdaptor adaptor,
                                     ConversionPatternRewriter &rewriter) const {
+  auto operands = adaptor.getOperands();
   if (operands.size() != 1)
     return rewriter.notifyMatchFailure(wrap, [&operands](Diagnostic &d) {
       d << "wrap.iface has 1 argument. Got " << operands.size() << "operands";
@@ -1042,14 +1042,15 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(UnwrapSVInterface wrap, ArrayRef<Value> operands,
+  matchAndRewrite(UnwrapSVInterface wrap, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult UnwrapInterfaceLower::matchAndRewrite(
-    UnwrapSVInterface unwrap, ArrayRef<Value> operands,
+    UnwrapSVInterface unwrap, OpAdaptor adaptor,
     ConversionPatternRewriter &rewriter) const {
+  auto operands = adaptor.getOperands();
   if (operands.size() != 2)
     return rewriter.notifyMatchFailure(unwrap, [&operands](Diagnostic &d) {
       d << "Unwrap.iface has 2 arguments. Got " << operands.size()
@@ -1088,7 +1089,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(CosimEndpoint, ArrayRef<Value> operands,
+  matchAndRewrite(CosimEndpoint, OpAdaptor operands,
                   ConversionPatternRewriter &rewriter) const final;
 
 private:
@@ -1097,7 +1098,7 @@ private:
 } // anonymous namespace
 
 LogicalResult
-CosimLowering::matchAndRewrite(CosimEndpoint ep, ArrayRef<Value> operands,
+CosimLowering::matchAndRewrite(CosimEndpoint ep, OpAdaptor operands,
                                ConversionPatternRewriter &rewriter) const {
 #ifndef CAPNP
   (void)builder;
@@ -1192,7 +1193,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(CapnpEncode enc, ArrayRef<Value> operands,
+  matchAndRewrite(CapnpEncode enc, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final {
 #ifndef CAPNP
     return rewriter.notifyMatchFailure(enc,
@@ -1219,7 +1220,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(CapnpDecode dec, ArrayRef<Value> operands,
+  matchAndRewrite(CapnpDecode dec, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final {
 #ifndef CAPNP
     return rewriter.notifyMatchFailure(dec,

lib/Conversion/HWToLLHD/HWToLLHD.cpp
@@ -91,7 +91,7 @@ HWToLLHDTypeConverter::HWToLLHDTypeConverter() {
   addConversion([](SigType type) { return type; });
 
   // Materialze probes when arguments are converted from any type to `SigType`.
-  addArgumentMaterialization(
+  addSourceMaterialization(
       [](OpBuilder &builder, Type type, ValueRange values, Location loc) {
         assert(values.size() == 1);
         auto op = builder.create<PrbOp>(loc, type, values[0]);
@@ -109,7 +109,7 @@ struct ConvertHWModule : public OpConversionPattern<HWModuleOp> {
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(HWModuleOp module, ArrayRef<Value> operands,
+  matchAndRewrite(HWModuleOp module, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     // Collect the HW module's port types.
     FunctionType moduleType = module.getType();
@@ -158,7 +158,7 @@ struct ConvertOutput : public OpConversionPattern<OutputOp> {
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(OutputOp output, ArrayRef<Value> operands,
+  matchAndRewrite(OutputOp output, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     // Get the number of inputs in the entity to offset into the block args.
     auto entity = output->getParentOfType<EntityOp>();
@@ -168,9 +168,9 @@ struct ConvertOutput : public OpConversionPattern<OutputOp> {
 
     // Drive the results from the mapped operands.
     Value delta;
-    for (size_t i = 0, e = operands.size(); i != e; ++i) {
+    for (size_t i = 0, e = adaptor.getOperands().size(); i != e; ++i) {
       // Get the source and destination signals.
-      auto src = operands[i];
+      auto src = adaptor.getOperands()[i];
       auto dest = entity.getArgument(numInputs + i);
       if (!src || !dest)
         return rewriter.notifyMatchFailure(
@@ -212,14 +212,14 @@ struct ConvertInstance : public OpConversionPattern<InstanceOp> {
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(InstanceOp instance, ArrayRef<Value> operands,
+  matchAndRewrite(InstanceOp instance, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     Value delta;
 
     // Materialize signals for instance arguments that are of non-signal type.
     SmallVector<Value, 4> arguments;
     unsigned argIdx = 0;
-    for (auto arg : operands) {
+    for (auto arg : adaptor.getOperands()) {
       // Connect signals directly.
       auto argType = arg.getType();
       if (argType.isa<SigType>()) {

lib/Conversion/HandshakeToFIRRTL/HandshakeToFIRRTL.cpp
@@ -2253,7 +2253,7 @@ struct HandshakeFuncOpLowering : public OpConversionPattern<handshake::FuncOp> {
       : OpConversionPattern<handshake::FuncOp>(context), circuitOp(circuitOp) {}
 
   LogicalResult
-  matchAndRewrite(handshake::FuncOp funcOp, ArrayRef<Value> operands,
+  matchAndRewrite(handshake::FuncOp funcOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     rewriter.setInsertionPointToStart(circuitOp.getBody());
     auto topModuleOp = createTopModuleOp(funcOp, /*numClocks=*/1, rewriter);

lib/Conversion/LLHDToLLVM/LLHDToLLVM.cpp
@@ -1888,11 +1888,12 @@ namespace {
 template <typename SourceOp, typename TargetOp>
 class VariadicOpConversion : public ConvertOpToLLVMPattern<SourceOp> {
 public:
+  using OpAdaptor = typename SourceOp::Adaptor;
   using ConvertOpToLLVMPattern<SourceOp>::ConvertOpToLLVMPattern;
   using Super = VariadicOpConversion<SourceOp, TargetOp>;
 
   LogicalResult
-  matchAndRewrite(SourceOp op, ArrayRef<Value> operands,
+  matchAndRewrite(SourceOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     size_t numOperands = op.getOperands().size();
@@ -1933,7 +1934,7 @@ struct BitcastOpConversion : public ConvertOpToLLVMPattern<hw::BitcastOp> {
   using ConvertOpToLLVMPattern<hw::BitcastOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::BitcastOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::BitcastOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type resultTy = typeConverter->convertType(op.result().getType());
@@ -2093,7 +2094,7 @@ struct HWArrayCreateOpConversion
   using ConvertOpToLLVMPattern<hw::ArrayCreateOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::ArrayCreateOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::ArrayCreateOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     auto arrayTy = typeConverter->convertType(op->getResult(0).getType());
@@ -2124,7 +2125,7 @@ struct HWStructCreateOpConversion
   using ConvertOpToLLVMPattern<hw::StructCreateOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::StructCreateOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::StructCreateOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     auto resTy = typeConverter->convertType(op.result().getType());
@@ -2158,7 +2159,7 @@ struct SigArraySliceOpConversion
   using ConvertOpToLLVMPattern<llhd::SigArraySliceOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(llhd::SigArraySliceOp op, ArrayRef<Value> operands,
+  matchAndRewrite(llhd::SigArraySliceOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type llvmArrTy = typeConverter->convertType(op.getInputArrayType());
@@ -2188,7 +2189,7 @@ struct SigExtractOpConversion
   using ConvertOpToLLVMPattern<llhd::SigExtractOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(llhd::SigExtractOp op, ArrayRef<Value> operands,
+  matchAndRewrite(llhd::SigExtractOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type inputTy = typeConverter->convertType(op.input().getType());
@@ -2226,7 +2227,7 @@ struct SigStructExtractOpConversion
       llhd::SigStructExtractOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(llhd::SigStructExtractOp op, ArrayRef<Value> operands,
+  matchAndRewrite(llhd::SigStructExtractOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type llvmStructTy = typeConverter->convertType(op.getStructType());
@@ -2263,7 +2264,7 @@ struct SigArrayGetOpConversion
   using ConvertOpToLLVMPattern<llhd::SigArrayGetOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(llhd::SigArrayGetOp op, ArrayRef<Value> operands,
+  matchAndRewrite(llhd::SigArrayGetOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     auto llvmArrTy = typeConverter->convertType(op.getArrayType());
@@ -2325,7 +2326,7 @@ struct StructExtractOpConversion
   using ConvertOpToLLVMPattern<hw::StructExtractOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::StructExtractOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::StructExtractOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type inputTy = typeConverter->convertType(op.input().getType());
@@ -2354,7 +2355,7 @@ struct ArrayGetOpConversion : public ConvertOpToLLVMPattern<hw::ArrayGetOp> {
   using ConvertOpToLLVMPattern<hw::ArrayGetOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::ArrayGetOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::ArrayGetOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     auto elemTy = typeConverter->convertType(op.result().getType());
@@ -2393,7 +2394,7 @@ struct ArraySliceOpConversion
   using ConvertOpToLLVMPattern<hw::ArraySliceOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::ArraySliceOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::ArraySliceOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     auto dstTy = typeConverter->convertType(op.dst().getType());
@@ -2443,7 +2444,7 @@ struct StructInjectOpConversion
   using ConvertOpToLLVMPattern<hw::StructInjectOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::StructInjectOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::StructInjectOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     Type inputTy = typeConverter->convertType(op.input().getType());
@@ -2476,7 +2477,7 @@ struct ArrayConcatOpConversion
   using ConvertOpToLLVMPattern<hw::ArrayConcatOp>::ConvertOpToLLVMPattern;
 
   LogicalResult
-  matchAndRewrite(hw::ArrayConcatOp op, ArrayRef<Value> operands,
+  matchAndRewrite(hw::ArrayConcatOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
     hw::ArrayType arrTy = op.result().getType().cast<hw::ArrayType>();

lib/Dialect/MSFT/MSFTPasses.cpp
@@ -43,14 +43,13 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(InstanceOp, ArrayRef<Value> operands,
+  matchAndRewrite(InstanceOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult
-InstanceOpLowering::matchAndRewrite(InstanceOp msftInst,
-                                    ArrayRef<Value> operands,
+InstanceOpLowering::matchAndRewrite(InstanceOp msftInst, OpAdaptor adaptor,
                                     ConversionPatternRewriter &rewriter) const {
   Operation *referencedModule = msftInst.getReferencedModule();
   if (!referencedModule)
@@ -61,7 +60,9 @@ InstanceOpLowering::matchAndRewrite(InstanceOp msftInst,
         msftInst, "Referenced module was not an HW module");
   auto hwInst = rewriter.create<hw::InstanceOp>(
       msftInst.getLoc(), referencedModule, msftInst.instanceNameAttr(),
-      operands, /*parameters=*/ArrayAttr{}, msftInst.sym_nameAttr());
+      SmallVector<Value>(adaptor.getOperands().begin(),
+                         adaptor.getOperands().end()),
+      /*parameters=*/ArrayAttr{}, msftInst.sym_nameAttr());
   rewriter.replaceOp(msftInst, hwInst.getResults());
   return success();
 }
@@ -73,13 +74,13 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(MSFTModuleOp mod, ArrayRef<Value> operands,
+  matchAndRewrite(MSFTModuleOp mod, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final;
 };
 } // anonymous namespace
 
 LogicalResult
-ModuleOpLowering::matchAndRewrite(MSFTModuleOp mod, ArrayRef<Value> operands,
+ModuleOpLowering::matchAndRewrite(MSFTModuleOp mod, OpAdaptor adaptor,
                                   ConversionPatternRewriter &rewriter) const {
   if (mod.body().empty()) {
     std::string comment;
@@ -106,7 +107,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(OutputOp out, ArrayRef<Value> operands,
+  matchAndRewrite(OutputOp out, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final {
     rewriter.replaceOpWithNewOp<hw::OutputOp>(out, out.getOperands());
     return success();

lib/Dialect/Seq/SeqPasses.cpp
@@ -38,7 +38,7 @@ public:
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(CompRegOp reg, ArrayRef<Value> operands,
+  matchAndRewrite(CompRegOp reg, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final {
     Location loc = reg.getLoc();
 

commit d8a418a045a36b48fde0ae0d6823ded77e0191c9

Fix build for CAPNP ESI
Fix problem introduced in daee6494 where regions that relied on use of
CAPNP were not properly updated with the LLVM bump.  This should fix
failing CI.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/ESI/ESIPasses.cpp
@@ -1098,7 +1098,7 @@ private:
 } // anonymous namespace
 
 LogicalResult
-CosimLowering::matchAndRewrite(CosimEndpoint ep, OpAdaptor operands,
+CosimLowering::matchAndRewrite(CosimEndpoint ep, OpAdaptor adaptor,
                                ConversionPatternRewriter &rewriter) const {
 #ifndef CAPNP
   (void)builder;
@@ -1107,6 +1107,7 @@ CosimLowering::matchAndRewrite(CosimEndpoint ep, OpAdaptor operands,
 #else
   auto loc = ep.getLoc();
   auto *ctxt = rewriter.getContext();
+  auto operands = adaptor.getOperands();
   Value clk = operands[0];
   Value rstn = operands[1];
   Value send = operands[2];
@@ -1203,6 +1204,7 @@ public:
     capnp::TypeSchema encodeType(enc.dataToEncode().getType());
     if (!encodeType.isSupported())
       return rewriter.notifyMatchFailure(enc, "Type not supported yet");
+    auto operands = adaptor.getOperands();
     Value encoderOutput = encodeType.buildEncoder(rewriter, operands[0],
                                                   operands[1], operands[2]);
     assert(encoderOutput && "Error in TypeSchema.buildEncoder()");
@@ -1230,6 +1232,7 @@ public:
     capnp::TypeSchema decodeType(dec.decodedData().getType());
     if (!decodeType.isSupported())
       return rewriter.notifyMatchFailure(dec, "Type not supported yet");
+    auto operands = adaptor.getOperands();
     Value decoderOutput = decodeType.buildDecoder(rewriter, operands[0],
                                                   operands[1], operands[2]);
     assert(decoderOutput && "Error in TypeSchema.buildDecoder()");