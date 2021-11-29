thomasnormal/circt

commit dcaf2d5fbf3b3a86f35510fda1703383e3d129e2

[CombFolds] Shrink icmp ult/ugt into an extract when possible.
This turns things like (icmp ugt x, 3) into (icmp ne (extract x, 2), 0).

This introduces extracts which compose well with width minimization
logic that exists.  This triggers a couple of times in RocketCore.fir.

This is part of Issue #2095

lib/Dialect/Comb/CombFolds.cpp
@@ -2444,7 +2444,6 @@ static void combineEqualityICmpWithXorOfConstant(ICmpOp cmpOp, XorOp xorOp,
   rewriter.replaceOpWithNewOp<ICmpOp>(cmpOp, cmpOp.predicate(), newLHS, newRHS);
 }
 
-// Canonicalizes a ICmp with a single constant
 LogicalResult ICmpOp::canonicalize(ICmpOp op, PatternRewriter &rewriter) {
   APInt lhs, rhs;
 
@@ -2498,25 +2497,48 @@ LogicalResult ICmpOp::canonicalize(ICmpOp op, PatternRewriter &rewriter) {
       break;
     case ICmpPredicate::ult:
       // x < max -> x != max
-      if (rhs.isMaxValue())
+      if (rhs.isAllOnes())
         return replaceWith(ICmpPredicate::ne, op.lhs(), op.rhs());
       // x < min -> false
-      if (rhs.isMinValue())
+      if (rhs.isZero())
         return replaceWithConstantI1(0);
       // x < min+1 -> x == min
-      if ((rhs - 1).isMinValue())
+      if ((rhs - 1).isZero())
         return replaceWith(ICmpPredicate::eq, op.lhs(), getConstant(rhs - 1));
+
+      // x < 0xF0 => extract(x, 0..3) != 0
+      if (rhs.countLeadingOnes() + rhs.countTrailingZeros() ==
+          rhs.getBitWidth()) {
+        auto numZeros = rhs.countTrailingZeros();
+        auto smallType = rewriter.getIntegerType(numZeros);
+        auto smaller =
+            rewriter.create<ExtractOp>(op.getLoc(), smallType, op.lhs(), 0);
+        return replaceWith(ICmpPredicate::ne, smaller,
+                           getConstant(APInt::getZero(numZeros)));
+      }
+
       break;
     case ICmpPredicate::ugt:
       // x > min -> x != min
-      if (rhs.isMinValue())
+      if (rhs.isZero())
         return replaceWith(ICmpPredicate::ne, op.lhs(), op.rhs());
       // x > max -> false
-      if (rhs.isMaxValue())
+      if (rhs.isAllOnes())
         return replaceWithConstantI1(0);
       // x > max-1 -> x == max
-      if ((rhs + 1).isMaxValue())
+      if ((rhs + 1).isAllOnes())
         return replaceWith(ICmpPredicate::eq, op.lhs(), getConstant(rhs + 1));
+
+      // x > 7 => extract(x, 3) != 0
+      if ((rhs + 1).isPowerOf2()) {
+        auto numOnes = rhs.countTrailingOnes();
+        auto smallType = rewriter.getIntegerType(rhs.getBitWidth() - numOnes);
+        auto smaller = rewriter.create<ExtractOp>(op.getLoc(), smallType,
+                                                  op.lhs(), numOnes);
+        return replaceWith(ICmpPredicate::ne, smaller,
+                           getConstant(APInt::getZero(smallType.getWidth())));
+      }
+
       break;
     case ICmpPredicate::sle:
       // x <= max -> true
@@ -2532,13 +2554,13 @@ LogicalResult ICmpOp::canonicalize(ICmpOp op, PatternRewriter &rewriter) {
       return replaceWith(ICmpPredicate::sgt, op.lhs(), getConstant(rhs - 1));
     case ICmpPredicate::ule:
       // x <= max -> true
-      if (rhs.isMaxValue())
+      if (rhs.isAllOnes())
         return replaceWithConstantI1(1);
       // x <= c -> x < (c+1)
       return replaceWith(ICmpPredicate::ult, op.lhs(), getConstant(rhs + 1));
     case ICmpPredicate::uge:
       // x >= min -> true
-      if (rhs.isMinValue())
+      if (rhs.isZero())
         return replaceWithConstantI1(1);
       // x >= c -> x > (c-1)
       return replaceWith(ICmpPredicate::ugt, op.lhs(), getConstant(rhs - 1));

test/Dialect/Comb/canonicalization.mlir
@@ -412,6 +412,23 @@ hw.module @compareConcatEliminationFalseCases(%arg0 : i4, %arg1: i9, %arg2: i7) 
   hw.output %o : i1
 }
 
+// CHECK-LABEL: @compareExtractFold
+hw.module @compareExtractFold(%arg0: i8) -> (o1: i1, o2: i1) {
+  %c3_i8 = hw.constant 3 : i8
+  %0 = comb.icmp ugt %arg0, %c3_i8 : i8
+  // CHECK: %0 = comb.extract %arg0 from 2 : (i8) -> i6
+  // CHECK: %1 = comb.icmp ne %0, %c0_i6 : i6 
+
+  // CHECK: %2 = comb.extract %arg0 from 0 : (i8) -> i6 
+  // CHECK: %3 = comb.icmp ne %2, %c0_i6 : i6 
+  %c192_i8 = hw.constant 192 : i8
+  %1 = comb.icmp ult %arg0, %c192_i8 : i8
+
+  // CHECK: hw.output %1, %3 :
+  hw.output %0, %1 : i1, i1
+}
+
+
 // Validates that extract(cat(a, b, c)) -> cat(b, c) when it aligns with the exact elements, or simply
 // a when it is a single full element.
 // CHECK-LABEL: hw.module @extractCatAlignWithExactElements
@@ -695,7 +712,6 @@ hw.module @narrow_extract_from_and(%arg0: i32) -> (o1: i8, o2: i14, o3: i8, o4: 
   hw.output %1, %2, %4, %6 : i8, i14, i8, i8
   // CHECK: hw.output %2, %1, %5, %7 : i8, i14, i8, i8
 }
-
 
 // CHECK-LABEL: hw.module @fold_mux_tree1
 hw.module @fold_mux_tree1(%sel: i2, %a: i8, %b: i8, %c: i8, %d: i8) -> (y: i8) {

commit cdfc7d24ec806e57dda20a541b4f506f734e2e2d

[CombFolds] Fix issue in icmp ult/ugt extract fold (#2250)
Fix an issue introduced in dcaf2d5 where ult comparisons against a
constant of the shape `0b111..111000..000` would be converted to an
extract operation. The fold is based on the observation that in an icmp
`x < cst`, where `cst` has this shape of a bunch of leading ones
followed by all zeros, all values for `x` for which the comparison would
return a 0 are the values that have all the upper bits set (those that
are a one in the constant). Therefore the comparison is equivalent to
checking if those upper bits are not all ones. The implementation in
dcaf2d5 erroneously checked the lower bits of the number.

lib/Dialect/Comb/CombFolds.cpp
@@ -2603,15 +2603,16 @@ LogicalResult ICmpOp::canonicalize(ICmpOp op, PatternRewriter &rewriter) {
       if ((rhs - 1).isZero())
         return replaceWith(ICmpPredicate::eq, op.lhs(), getConstant(rhs - 1));
 
-      // x < 0xF0 => extract(x, 0..3) != 0
+      // x < 0xE0 -> extract(x, 5..7) != 0b111
       if (rhs.countLeadingOnes() + rhs.countTrailingZeros() ==
           rhs.getBitWidth()) {
-        auto numZeros = rhs.countTrailingZeros();
-        auto smallType = rewriter.getIntegerType(numZeros);
-        auto smaller =
-            rewriter.create<ExtractOp>(op.getLoc(), smallType, op.lhs(), 0);
-        return replaceWith(ICmpPredicate::ne, smaller,
-                           getConstant(APInt::getZero(numZeros)));
+        auto numOnes = rhs.countLeadingOnes();
+        auto smallType = rewriter.getIntegerType(numOnes);
+        auto smaller = rewriter.create<ExtractOp>(
+            op.getLoc(), smallType, op.lhs(), rhs.getBitWidth() - numOnes);
+        return replaceWith(
+            ICmpPredicate::ne, smaller,
+            getConstant(APInt::getAllOnes(smallType.getWidth())));
       }
 
       break;
@@ -2626,7 +2627,7 @@ LogicalResult ICmpOp::canonicalize(ICmpOp op, PatternRewriter &rewriter) {
       if ((rhs + 1).isAllOnes())
         return replaceWith(ICmpPredicate::eq, op.lhs(), getConstant(rhs + 1));
 
-      // x > 7 => extract(x, 3) != 0
+      // x > 0x07 -> extract(x, 3..7) != 0b00000
       if ((rhs + 1).isPowerOf2()) {
         auto numOnes = rhs.countTrailingOnes();
         auto smallType = rewriter.getIntegerType(rhs.getBitWidth() - numOnes);

test/Dialect/Comb/canonicalization.mlir
@@ -413,19 +413,27 @@ hw.module @compareConcatEliminationFalseCases(%arg0 : i4, %arg1: i9, %arg2: i7) 
 }
 
 // CHECK-LABEL: @compareExtractFold
-hw.module @compareExtractFold(%arg0: i8) -> (o1: i1, o2: i1) {
+hw.module @compareExtractFold(%arg0: i8) -> (o1: i1, o2: i1, o3: i1) {
+  // x > 0b00000011 -> extract(x, 2..8) != 0b000000
   %c3_i8 = hw.constant 3 : i8
   %0 = comb.icmp ugt %arg0, %c3_i8 : i8
   // CHECK: %0 = comb.extract %arg0 from 2 : (i8) -> i6
   // CHECK: %1 = comb.icmp ne %0, %c0_i6 : i6 
 
-  // CHECK: %2 = comb.extract %arg0 from 0 : (i8) -> i6 
-  // CHECK: %3 = comb.icmp ne %2, %c0_i6 : i6 
+  // x < 0b11000000 -> extract(x, 6..8) != 0b11
   %c192_i8 = hw.constant 192 : i8
   %1 = comb.icmp ult %arg0, %c192_i8 : i8
+  // CHECK: %2 = comb.extract %arg0 from 6 : (i8) -> i2
+  // CHECK: %3 = comb.icmp ne %2, %c-1_i2 : i2
 
-  // CHECK: hw.output %1, %3 :
-  hw.output %0, %1 : i1, i1
+  // The following used to be erroneously folded to `%arg0`.
+  %c0_i2 = hw.constant 0 : i2
+  %2 = comb.concat %c0_i2, %arg0 : i2, i8
+  %c768_i10 = hw.constant 768 : i10
+  %3 = comb.icmp ult %2, %c768_i10 : i10
+
+  // CHECK: hw.output %1, %3, %true :
+  hw.output %0, %1, %3 : i1, i1, i1
 }
 
 