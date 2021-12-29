thomasnormal/circt

commit 2fe34af3a08c5a0980f149679076cb34a3484d88

[ExportVerilog] Fix a super obscure bug where we didn't indent temps right.
In an `ifdef region, when spilling a temporary, we'd indent at the level
of the ifdef instead of the correct level of the definition, leading to
things like this (taken from the example in Issue #2310):

```
module Foo(
  input  clock, d,
  output q);

  reg r;

  `ifndef SYNTHESIS
    reg [31:0] _T;

  wire _T_0 = _T[0];   /// <--- indented wrong
    initial begin
```

The "unindent" behavior was needed when spilling due to lines too long,
because the decls are put outside the `ifdef region.  To solve this,
just keep track of the indent depth.

lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -2419,6 +2419,7 @@ private:
   /// This is the index of the end of the declaration region of the current
   /// 'begin' block, used to emit variable declarations.
   RearrangableOStream::Cursor blockDeclarationInsertPoint;
+  unsigned blockDeclarationIndentLevel = 0;
 
   /// This keeps track of the number of statements emitted, important for
   /// determining if we need to put out a begin/end marker in a block
@@ -2985,6 +2986,9 @@ void StmtEmitter::emitBlockAsStatement(Block *block,
   // block, and restore it back when we move on to code after the block.
   llvm::SaveAndRestore<RearrangableOStream::Cursor> X(
       blockDeclarationInsertPoint, rearrangableStream.getCursor());
+  llvm::SaveAndRestore<unsigned> X2(blockDeclarationIndentLevel,
+                                    state.currentIndent + INDENT_AMOUNT);
+
   auto numEmittedBefore = getNumStatementsEmitted();
   emitStatementBlock(*block);
 
@@ -3489,18 +3493,7 @@ isExpressionEmittedInlineIntoProceduralDeclaration(Operation *op,
 bool StmtEmitter::emitDeclarationForTemporary(Operation *op) {
   StringRef declWord = getVerilogDeclWord(op, state.options);
 
-  // If we're emitting a declaration inside of an ifdef region, we'll insert
-  // the declaration outside of it.  This means we need to unindent a bit due
-  // to the indent level.
-  unsigned ifdefDepth = 0;
-  Operation *parentOp = op->getParentOp();
-  while (isa<IfDefProceduralOp>(parentOp) || isa<IfDefOp>(parentOp)) {
-    ++ifdefDepth;
-    parentOp = parentOp->getParentOp();
-  }
-
-  os.indent(state.currentIndent - ifdefDepth * INDENT_AMOUNT);
-  os << declWord;
+  os.indent(blockDeclarationIndentLevel) << declWord;
   if (!declWord.empty())
     os << ' ';
   if (emitter.printPackedType(stripUnpackedTypes(op->getResult(0).getType()),
@@ -3601,6 +3594,7 @@ void StmtEmitter::collectNamesEmitDecls(Block &block) {
     // This is important to maintain incrementally after each statement, because
     // each statement can generate spills when they are overly-long.
     blockDeclarationInsertPoint = rearrangableStream.getCursor();
+    blockDeclarationIndentLevel = state.currentIndent;
   }
 
   os << '\n';

lib/Conversion/ExportVerilog/PrepareForEmission.cpp
@@ -254,6 +254,9 @@ static Operation *rewriteAddWithNegativeConstant(comb::AddOp add,
 
 /// Given an operation in a procedural region, scan up the region tree to find
 /// the first operation in a graph region (typically an always or initial op).
+///
+/// By looking for a graph region, we will stop at graph-region #ifdef's that
+/// may enclose this operation.
 static Operation *findParentInNonProceduralRegion(Operation *op) {
   Operation *parentOp = op->getParentOp();
   assert(parentOp->hasTrait<ProceduralRegion>() &&

test/Conversion/ExportVerilog/disallow-local-vars.mlir
@@ -1,5 +1,5 @@
 // RUN: circt-opt --export-verilog %s | FileCheck %s
-// RUN: circt-opt --lowering-options=disallowLocalVariables --export-verilog %s | FileCheck %s --check-prefix=DISALLOW
+// RUN: circt-opt --lowering-options=disallowLocalVariables --export-verilog %s | FileCheck %s --check-prefix=DISALLOW -strict-whitespace
 
 // This checks ExportVerilog's support for "disallowLocalVariables" which
 // prevents emitting 'automatic logic' and other local declarations.
@@ -8,31 +8,36 @@
 // DISALLOW-LABEL: module side_effect_expr
 hw.module @side_effect_expr(%clock: i1) -> (a: i1, a2: i1) {
 
-  // DISALLOW: reg [[SE_REG:[_A-Za-z0-9]+]];
+  // CHECK: `ifdef FOO_MACRO
+  // DISALLOW: `ifdef FOO_MACRO
+  sv.ifdef "FOO_MACRO" {
+    // DISALLOW: {{^    }}reg [[SE_REG:[_A-Za-z0-9]+]];
+    // DISALLOW: {{^    }}wire [[COND:[_A-Za-z0-9]+]] = INLINE_OK;
 
-  // DISALLOW: wire [[COND:[_A-Za-z0-9]+]] = INLINE_OK;
+    // CHECK:    always @(posedge clock)
+    // DISALLOW: always @(posedge clock)
+    sv.always posedge %clock  {
+      %0 = sv.verbatim.expr "INLINE_OK" : () -> i1
 
-  // CHECK:    always @(posedge clock)
-  // DISALLOW: always @(posedge clock)
-  sv.always posedge %clock  {
-    %0 = sv.verbatim.expr "INLINE_OK" : () -> i1
+      // This shouldn't be pushed into a reg.
+      // CHECK: if (INLINE_OK)
+      // DISALLOW: if ([[COND]])
+      sv.if %0  {
+        sv.fatal 1
+      }
 
-    // This shouldn't be pushed into a reg.
-    // CHECK: if (INLINE_OK)
-    // DISALLOW: if ([[COND]])
-    sv.if %0  {
-      sv.fatal 1
-    }
-
-    // This should go through a reg when in "disallow" mode.
-    // CHECK: if (SIDE_EFFECT)
-    // DISALLOW: [[SE_REG]] = SIDE_EFFECT;
-    // DISALLOW: if ([[SE_REG]])
-    %1 = sv.verbatim.expr.se "SIDE_EFFECT" : () -> i1
-    sv.if %1  {
-      sv.fatal 1
+      // This should go through a reg when in "disallow" mode.
+      // CHECK: if (SIDE_EFFECT)
+      // DISALLOW: [[SE_REG]] = SIDE_EFFECT;
+      // DISALLOW: if ([[SE_REG]])
+      %1 = sv.verbatim.expr.se "SIDE_EFFECT" : () -> i1
+      sv.if %1  {
+        sv.fatal 1
+      }
     }
   }
+  // CHECK: `endif
+  // DISALLOW: `endif
 
   // Top level things should go unmodified.
   %2 = sv.verbatim.expr "NO_SE" : () -> i1

commit 01fecea13e95f78b775fd889f2ba32f933999c3c

[ExportVerilog] Fix wire indent level at top level, Issue #2363
This was a bug introduced in 2fe34af.  Thanks to Hideto for the
nice testcase!

lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -2467,7 +2467,7 @@ private:
   /// This is the index of the end of the declaration region of the current
   /// 'begin' block, used to emit variable declarations.
   RearrangableOStream::Cursor blockDeclarationInsertPoint;
-  unsigned blockDeclarationIndentLevel = 0;
+  unsigned blockDeclarationIndentLevel = INDENT_AMOUNT;
 
   /// This keeps track of the number of statements emitted, important for
   /// determining if we need to put out a begin/end marker in a block

test/Conversion/ExportVerilog/hw-dialect.mlir
@@ -999,3 +999,14 @@ hw.module @parameterizedTypes<param: i32 = 1, wire: i32 = 2>
 // CHECK-NEXT:  module moduleWithComment
 hw.module @moduleWithComment()
   attributes {comment = "moduleWithComment has a comment\nhello"} {}
+
+
+// CHECK-LABEL: module Foo(
+// https://github.com/llvm/circt/issues/2363
+hw.module @Foo(%a: i1, %b: i1) -> (r1: i1, r2: i1) {
+  // Make sure the temporary wire is indented correctly.
+  // CHECK: {{^  wire _T = a == b;}}
+  %0 = comb.icmp eq %a, %b : i1
+  hw.output %0, %0 : i1, i1
+}
+