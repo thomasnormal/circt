thomasnormal/circt

commit 63960fc53edb35baf3e7f4e96a65600a1139ee1c

[FIRRTL]  Don't lower types for invalid durring parsing. (#2731)
Let the parser keep invalids of aggregate type.  Teach LowerTypes about StrictConnect and have it generate them when possible.

lib/Dialect/FIRRTL/Import/FIRParser.cpp
@@ -1636,7 +1636,17 @@ private:
 void FIRStmtParser::emitInvalidate(Value val, Flow flow) {
   auto tpe = val.getType().cast<FIRRTLType>();
 
-  // Recurse until we hit leaves.  Connect any leaves which have sink or
+  if (tpe.isPassive()) {
+    if (flow == Flow::Source || tpe.isa<AnalogType>())
+      return;
+    if (tpe.hasUninferredWidth())
+      builder.create<ConnectOp>(val, builder.create<InvalidValueOp>(tpe));
+    else
+      builder.create<StrictConnectOp>(val, builder.create<InvalidValueOp>(tpe));
+    return;
+  }
+
+  // Recurse until we hit passive leaves.  Connect any leaves which have sink or
   // duplex flow.
   //
   // TODO: This is very similar to connect expansion in the LowerTypes pass
@@ -1667,13 +1677,6 @@ void FIRStmtParser::emitInvalidate(Value val, Flow flow) {
           }
           emitInvalidate(subindex, flow);
         }
-      })
-      // Drop invalidation of analog.
-      .Case<AnalogType>([](auto) {})
-      // Invalidate any sink or duplex flow ground types.
-      .Default([&](auto tpe) {
-        if (flow != Flow::Source)
-          builder.create<ConnectOp>(val, builder.create<InvalidValueOp>(tpe));
       });
 }
 

test/Dialect/FIRRTL/parse-basic.fir
@@ -91,7 +91,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     _t <- _t_2
 
     ; CHECK: [[INV:%.+]]  = firrtl.invalidvalue : !firrtl.uint<1>
-    ; CHECK-NEXT: firrtl.connect %auto, [[INV]] : !firrtl.uint<1>, !firrtl.uint<1>
+    ; CHECK-NEXT: firrtl.strictconnect %auto, [[INV]] : !firrtl.uint<1>
     auto is invalid
 
     ; CHECK-NOT: firrtl.attach %a1
@@ -101,7 +101,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     skip  @[SKipLoc.scala 42:24]
 
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue : !firrtl.uint<1>
-    ; CHECK-NEXT: firrtl.connect %auto, [[INV]] : !firrtl.uint<1>, !firrtl.uint<1>
+    ; CHECK-NEXT: firrtl.strictconnect %auto, [[INV]] : !firrtl.uint<1>
     auto is invalid
 
     ; CHECK-NOT: firrtl.connect %reset
@@ -550,13 +550,11 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
 
     ; Check that invalidation reuses subfields
     wire w: {a: UInt<1>}[1]
-    ; CHECK: %1 = firrtl.subindex %w[0]
-    ; CHECK: %2 = firrtl.subfield %1(0)
-    ; CHECK: %invalid_ui1 = firrtl.invalidvalue
-    ; CHECK: firrtl.connect %2, %invalid_ui1
+    ; CHECK: %invalid = firrtl.invalidvalue : !firrtl.vector<bundle<a: uint<1>>, 1>
+    ; CHECK: firrtl.strictconnect %w, %invalid
     w is invalid
-    ; CHECK: %invalid_ui1_0 = firrtl.invalidvalue
-    ; CHECK: firrtl.connect %2, %invalid_ui1_0
+    ; CHECK: %invalid_0 = firrtl.invalidvalue : !firrtl.vector<bundle<a: uint<1>>, 1>
+    ; CHECK: firrtl.strictconnect %w, %invalid_0
     w is invalid
 
   ; CHECK-LABEL: firrtl.module @flip_one
@@ -696,7 +694,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     input in2 : { a : UInt<1>, flip b : UInt<1>}
     ; CHECK: [[IN2_B:%.+]] = firrtl.subfield %in2(1)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[IN2_B]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[IN2_B]], [[INV]]
     in2 is invalid
 
   module CheckInvalids_in3 :
@@ -704,30 +702,26 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     ; CHECK: [[IN3_A:%.+]] = firrtl.subfield %in3(0)
     ; CHECK: [[IN3_A_C:%.+]] = firrtl.subfield [[IN3_A]](1)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[IN3_A_C]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[IN3_A_C]], [[INV]]
     in3 is invalid
 
   module CheckInvalids_out0 :
     output out0 : UInt<1>
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect %out0, [[INV]]
+    ; CHECK: firrtl.strictconnect %out0, [[INV]]
     out0 is invalid
 
   module CheckInvalids_out1 :
     output out1 : { a : UInt<1>, b : UInt<1> }
-    ; CHECK: [[OUT1_B:%.+]] = firrtl.subfield %out1(1)
-    ; CHECK: [[OUT1_A:%.+]] = firrtl.subfield %out1(0)
-    ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[OUT1_A]], [[INV]]
-    ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[OUT1_B]], [[INV]]
+    ; CHECK: [[INV:%.+]] = firrtl.invalidvalue : !firrtl.bundle<a: uint<1>, b: uint<1>> 
+    ; CHECK: firrtl.strictconnect %out1, [[INV]]
     out1 is invalid
 
   module CheckInvalids_out2 :
     output out2 : { a : UInt<1>, flip b : UInt<1>}
     ; CHECK: [[OUT2_A:%.+]] = firrtl.subfield %out2(0)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[OUT2_A]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[OUT2_A]], [[INV]]
     out2 is invalid
 
   module CheckInvalids_out3 :
@@ -735,13 +729,13 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     ; CHECK: [[OUT3_A:%.+]] = firrtl.subfield %out3(0)
     ; CHECK: [[OUT3_A_B:%.+]] = firrtl.subfield [[OUT3_A]](0)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[OUT3_A_B]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[OUT3_A_B]], [[INV]]
     out3 is invalid
 
   module CheckInvalids_wires :
     ; CHECK: %wire0 = firrtl.wire
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect %wire0, [[INV]]
+    ; CHECK: firrtl.strictconnect %wire0, [[INV]]
     wire wire0 : UInt<1>
     wire0 is invalid
 
@@ -749,9 +743,9 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     ; CHECK: [[WIRE1_B:%.+]] = firrtl.subfield %wire1(1)
     ; CHECK: [[WIRE1_A:%.+]] = firrtl.subfield %wire1(0)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[WIRE1_A]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[WIRE1_A]], [[INV]]
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[WIRE1_B]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[WIRE1_B]], [[INV]]
     wire wire1 : {a : UInt<1>, flip b : UInt<1> }
     wire1 is invalid
 
@@ -761,7 +755,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     ; CHECK: [[WIRE2_X_B:%.+]] = firrtl.subfield [[WIRE2_X]](1)
     ; CHECK: [[WIRE2_X_A:%.+]] = firrtl.subfield [[WIRE2_X]](0)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[WIRE2_X_A]], [[INV]]
+    ; CHECK: firrtl.strictconnect [[WIRE2_X_A]], [[INV]]
     ; CHECK-NOT: firrtl.attach [[WIRE2_X_B]], [[INV]]
     wire wire2 : {x : {flip a : UInt<1>, flip b: Analog<1> } }
     wire2 is invalid
@@ -770,11 +764,10 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     ; CHECK: %U0_in0, %U0_in1, %U0_out0, %U0_out1 = firrtl.instance U0 @mod_0_563
     inst U0 of mod_0_563
 
-    ; CHECK: [[U0_IN1_A:%.+]] = firrtl.subfield %U0_in1(0)
     ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect %U0_in0, [[INV]]
-    ; CHECK: [[INV:%.+]] = firrtl.invalidvalue
-    ; CHECK: firrtl.connect [[U0_IN1_A]], [[INV]]
+    ; CHECK: firrtl.strictconnect %U0_in0, [[INV]]
+    ; CHECK: [[INV:%.+]] = firrtl.invalidvalue : !firrtl.bundle<a: uint<5>> 
+    ; CHECK: firrtl.strictconnect %U0_in1, [[INV]]
     U0 is invalid
 
   ; This reference is declared after its first use.

include/circt/Dialect/FIRRTL/FIRRTLStatements.td
@@ -68,7 +68,7 @@ def StrictConnectOp : FIRRTLOp<"strictconnect", [SameTypeOperands]> {
     ```
     }];
 
-  let arguments = (ins FIRRTLType:$dest, FIRRTLType:$src);
+  let arguments = (ins SizedType:$dest, SizedType:$src);
   let results = (outs);
   let hasCanonicalizeMethod = true;
 

include/circt/Dialect/FIRRTL/FIRRTLTypes.td
@@ -39,6 +39,11 @@ def BundleType : DialectType<FIRRTLDialect, CPred<"$_self.isa<BundleType>()">,
 def FVectorType : DialectType<FIRRTLDialect, CPred<"$_self.isa<FVectorType>()">,
   "FVectorType", "::circt::firrtl::FVectorType">;
 
+def SizedType : DialectType<FIRRTLDialect,
+    CPred<"$_self.isa<FIRRTLType>() && "
+          "!$_self.cast<FIRRTLType>().hasUninferredWidth()">,
+    "a sized type (contains no unifered widths)", "::circt::firrtl::FIRRTLType">;
+
 def UInt1Type : DialectType<FIRRTLDialect,
     CPred<"$_self.isa<UIntType>() && "
           "($_self.cast<UIntType>().getWidth() == 1 ||"

lib/Dialect/FIRRTL/Export/FIREmitter.cpp
@@ -65,6 +65,7 @@ struct Emitter {
   void emitStatement(SkipOp op);
   void emitStatement(PrintFOp op);
   void emitStatement(ConnectOp op);
+  void emitStatement(StrictConnectOp op);
   void emitStatement(PartialConnectOp op);
   void emitStatement(InstanceOp op);
   void emitStatement(AttachOp op);
@@ -315,7 +316,7 @@ void Emitter::emitStatementsInBlock(Block &block) {
       continue;
     TypeSwitch<Operation *>(&bodyOp)
         .Case<WhenOp, WireOp, RegOp, RegResetOp, NodeOp, StopOp, SkipOp,
-              PrintFOp, AssertOp, AssumeOp, CoverOp, ConnectOp,
+              PrintFOp, AssertOp, AssumeOp, CoverOp, ConnectOp, StrictConnectOp,
               PartialConnectOp, InstanceOp, AttachOp, MemOp, InvalidValueOp,
               SeqMemOp, CombMemOp, MemoryPortOp, MemoryPortAccessOp>(
             [&](auto op) { emitStatement(op); })
@@ -447,6 +448,18 @@ void Emitter::emitVerifStatement(T op, StringRef mnemonic) {
 }
 
 void Emitter::emitStatement(ConnectOp op) {
+  indent();
+  emitExpression(op.dest());
+  if (op.src().getDefiningOp<InvalidValueOp>()) {
+    os << " is invalid";
+  } else {
+    os << " <= ";
+    emitExpression(op.src());
+  }
+  emitLocationAndNewLine(op);
+}
+
+void Emitter::emitStatement(StrictConnectOp op) {
   indent();
   emitExpression(op.dest());
   if (op.src().getDefiningOp<InvalidValueOp>()) {

lib/Dialect/FIRRTL/FIRRTLFolds.cpp
@@ -1444,8 +1444,7 @@ static LogicalResult canonicalizeSingleSetConnect(StrictConnectOp op,
     return failure();
 
   // Only forward if the types exactly match and there is one connect.
-  if (op.dest().getType() != op.src().getType() ||
-      getSingleConnectUserOf(op.dest()) != op)
+  if (getSingleConnectUserOf(op.dest()) != op)
     return failure();
 
   // Only do this if the connectee and the declaration are in the same block.
@@ -1471,7 +1470,9 @@ static LogicalResult canonicalizeSingleSetConnect(StrictConnectOp op,
   if (srcValueOp) {
     // Replace with constant zero.
     if (isa<InvalidValueOp>(srcValueOp)) {
-      if (op.dest().getType().isa<ClockType, AsyncResetType, ResetType>())
+      if (op.dest().getType().isa<BundleType>())
+        failure();
+      else if (op.dest().getType().isa<ClockType, AsyncResetType, ResetType>())
         replacement = rewriter.create<SpecialConstantOp>(
             op.src().getLoc(), op.dest().getType(),
             rewriter.getBoolAttr(false));

lib/Dialect/FIRRTL/Transforms/InferWidths.cpp
@@ -1411,7 +1411,7 @@ LogicalResult InferenceMapping::mapOperation(Operation *op) {
       })
 
       // Handle the various connect statements that imply a type constraint.
-      .Case<ConnectOp>([&](auto op) {
+      .Case<ConnectOp, StrictConnectOp>([&](auto op) {
         // If the source is an invalid value, we don't set a constraint between
         // these two types.
         if (dyn_cast_or_null<InvalidValueOp>(op.src().getDefiningOp()))
@@ -1794,7 +1794,8 @@ bool InferenceTypeUpdate::updateOperation(Operation *op) {
   // operand width will have definitely been mapped in.
   if (isa<InvalidValueOp>(op) &&
       hasUninferredWidth(op->getResultTypes().front())) {
-    if (op->use_empty() || !isa<ConnectOp>(*op->getUsers().begin())) {
+    if (op->use_empty() ||
+        !isa<ConnectOp, StrictConnectOp>(*op->getUsers().begin())) {
       auto diag = mlir::emitError(
           op->getLoc(), "uninferred width: invalid value is unconstrained");
       anyFailed = true;

lib/Dialect/FIRRTL/Transforms/LowerCHIRRTL.cpp
@@ -44,6 +44,7 @@ struct LowerCHIRRTLPass : public LowerCHIRRTLPassBase<LowerCHIRRTLPass>,
   void visitExpr(SubfieldOp op);
   void visitExpr(SubindexOp op);
   void visitStmt(ConnectOp op);
+  void visitStmt(StrictConnectOp op);
   void visitStmt(PartialConnectOp op);
   void visitUnhandledOp(Operation *op);
 
@@ -121,6 +122,13 @@ struct LowerCHIRRTLPass : public LowerCHIRRTLPassBase<LowerCHIRRTLPass>,
 };
 } // end anonymous namespace
 
+static void mkConnect(ImplicitLocOpBuilder &builder, Value dst, Value src) {
+  if (dst.getType() == src.getType())
+    builder.create<StrictConnectOp>(dst, src);
+  else
+    builder.create<ConnectOp>(dst, src);
+}
+
 /// Performs the callback for each leaf element of a value.  This will create
 /// any subindex and subfield operations needed to access the leaf values of the
 /// aggregate value.
@@ -144,7 +152,7 @@ static void forEachLeaf(ImplicitLocOpBuilder &builder, Value value,
 static void connectLeafsTo(ImplicitLocOpBuilder &builder, Value bundle,
                            Value value) {
   forEachLeaf(builder, bundle,
-              [&](Value leaf) { builder.create<ConnectOp>(leaf, value); });
+              [&](Value leaf) { mkConnect(builder, leaf, value); });
 }
 
 /// Connect each leaf of an aggregate type to invalid.  This does not support
@@ -156,7 +164,7 @@ void LowerCHIRRTLPass::emitInvalid(ImplicitLocOpBuilder &builder, Value value) {
     auto builder = OpBuilder::atBlockBegin(getOperation().getBody());
     invalid = builder.create<InvalidValueOp>(getOperation().getLoc(), type);
   }
-  builder.create<ConnectOp>(value, invalid);
+  mkConnect(builder, value, invalid);
 }
 
 /// Converts a CHIRRTL memory port direction to a MemoryOp port type.  The
@@ -241,6 +249,12 @@ MemDirAttr LowerCHIRRTLPass::inferMemoryPortKind(MemoryPortOp memPort) {
         // Otherwise we are reading from a memory for the index.
         element.mode |= MemDirAttr::Read;
       } else if (auto connectOp = dyn_cast<ConnectOp>(user)) {
+        if (use.get() == connectOp.dest()) {
+          element.mode |= MemDirAttr::Write;
+        } else {
+          element.mode |= MemDirAttr::Read;
+        }
+      } else if (auto connectOp = dyn_cast<StrictConnectOp>(user)) {
         if (use.get() == connectOp.dest()) {
           element.mode |= MemDirAttr::Write;
         } else {
@@ -362,7 +376,7 @@ void LowerCHIRRTLPass::replaceMem(Operation *cmem, StringRef name,
     auto address = memBuilder.create<SubfieldOp>(memoryPort, "addr");
     emitInvalid(memBuilder, address);
     auto enable = memBuilder.create<SubfieldOp>(memoryPort, "en");
-    memBuilder.create<ConnectOp>(enable, getConst(0));
+    mkConnect(memBuilder, enable, getConst(0));
     auto clock = memBuilder.create<SubfieldOp>(memoryPort, "clk");
     emitInvalid(memBuilder, clock);
 
@@ -375,13 +389,12 @@ void LowerCHIRRTLPass::replaceMem(Operation *cmem, StringRef name,
         addressLHS.getBitWidthOrSentinel() < addressRHS.getBitWidthOrSentinel())
       portBuilder.create<PartialConnectOp>(address, cmemoryPortAccess.index());
     else
-      portBuilder.create<ConnectOp>(address, cmemoryPortAccess.index());
+      mkConnect(portBuilder, address, cmemoryPortAccess.index());
     // Sequential+Read ports have a more complicated "enable inference".
     // Everything else sets the enable to true.
-    if (!(portKind == MemOp::PortKind::Read && isSequential)) {
-      portBuilder.create<ConnectOp>(enable, getConst(1));
-    }
-    portBuilder.create<ConnectOp>(clock, cmemoryPortAccess.clock());
+    if (!(portKind == MemOp::PortKind::Read && isSequential))
+      mkConnect(portBuilder, enable, getConst(1));
+    mkConnect(portBuilder, clock, cmemoryPortAccess.clock());
 
     if (portKind == MemOp::PortKind::Read) {
       // Store the read information for updating subfield ops.
@@ -403,7 +416,7 @@ void LowerCHIRRTLPass::replaceMem(Operation *cmem, StringRef name,
       // Initialization at the MemoryOp.
       auto rdata = memBuilder.create<SubfieldOp>(memoryPort, "rdata");
       auto wmode = memBuilder.create<SubfieldOp>(memoryPort, "wmode");
-      memBuilder.create<ConnectOp>(wmode, getConst(0));
+      mkConnect(memBuilder, wmode, getConst(0));
       auto wdata = memBuilder.create<SubfieldOp>(memoryPort, "wdata");
       emitInvalid(memBuilder, wdata);
       auto wmask = memBuilder.create<SubfieldOp>(memoryPort, "wmask");
@@ -441,6 +454,10 @@ void LowerCHIRRTLPass::replaceMem(Operation *cmem, StringRef name,
                 if (cmemoryPortAccess.index() == connectOp.dest())
                   return !dyn_cast_or_null<InvalidValueOp>(
                       connectOp.src().getDefiningOp());
+              } else if (auto connectOp = dyn_cast<StrictConnectOp>(op)) {
+                if (cmemoryPortAccess.index() == connectOp.dest())
+                  return !dyn_cast_or_null<InvalidValueOp>(
+                      connectOp.src().getDefiningOp());
               } else if (auto pConnectOp = dyn_cast<PartialConnectOp>(op)) {
                 if (cmemoryPortAccess.index() == pConnectOp.dest())
                   return !dyn_cast_or_null<InvalidValueOp>(
@@ -451,15 +468,15 @@ void LowerCHIRRTLPass::replaceMem(Operation *cmem, StringRef name,
 
         // At each location where we drive a value to the index, set the enable.
         for (auto *driver : drivers) {
-          OpBuilder(driver).create<ConnectOp>(driver->getLoc(), enable,
-                                              getConst(1));
+          OpBuilder(driver).create<StrictConnectOp>(driver->getLoc(), enable,
+                                                    getConst(1));
           success = true;
         }
       } else if (isa<NodeOp>(indexOp)) {
         // If using a Node for the address, then the we place the enable at the
         // Node op's
-        OpBuilder(indexOp).create<ConnectOp>(indexOp->getLoc(), enable,
-                                             getConst(1));
+        OpBuilder(indexOp).create<StrictConnectOp>(indexOp->getLoc(), enable,
+                                                   getConst(1));
         success = true;
       }
 
@@ -502,7 +519,30 @@ void LowerCHIRRTLPass::visitStmt(ConnectOp connect) {
     connectLeafsTo(builder, writeData.mask, getConst(1));
     // Only ReadWrite memories have a write mode.
     if (writeData.mode)
-      builder.create<ConnectOp>(writeData.mode, getConst(1));
+      mkConnect(builder, writeData.mode, getConst(1));
+  }
+  // Check if we are reading from a memory and, if we are, replace the
+  // source.
+  auto readIt = rdataValues.find(connect.src());
+  if (readIt != rdataValues.end()) {
+    auto newSource = readIt->second;
+    connect.srcMutable().assign(newSource);
+  }
+}
+
+void LowerCHIRRTLPass::visitStmt(StrictConnectOp connect) {
+  // Check if we are writing to a memory and, if we are, replace the
+  // destination.
+  auto writeIt = wdataValues.find(connect.dest());
+  if (writeIt != wdataValues.end()) {
+    auto writeData = writeIt->second;
+    connect.destMutable().assign(writeData.data);
+    // Assign the write mask.
+    ImplicitLocOpBuilder builder(connect.getLoc(), connect);
+    connectLeafsTo(builder, writeData.mask, getConst(1));
+    // Only ReadWrite memories have a write mode.
+    if (writeData.mode)
+      mkConnect(builder, writeData.mode, getConst(1));
   }
   // Check if we are reading from a memory and, if we are, replace the
   // source.
@@ -572,7 +612,7 @@ void LowerCHIRRTLPass::emitPartialConnectMask(
   } else {
     // Connect the mask to 1, forcing the creation of any required subfield and
     // subindex operations.
-    builder.create<ConnectOp>(getSubaccess(builder), getConst(1));
+    mkConnect(builder, getSubaccess(builder), getConst(1));
   }
 }
 
@@ -597,7 +637,7 @@ void LowerCHIRRTLPass::visitStmt(PartialConnectOp partialConnect) {
     // Only ReadWrite memories have a write mode, so this field can sometimes be
     // null.
     if (writeData.mode)
-      builder.create<ConnectOp>(writeData.mode, getConst(1));
+      mkConnect(builder, writeData.mode, getConst(1));
   }
 
   // Check if we are reading from a memory and, if we are, replace the

commit 76c0d88148833165127b1d7938d31f772fbf862d

[FIRParser] Don't bulk connect bundles with analog in them (#2778)
This is a small regression from 63960fc53edb. When an analog is inside a
bundle which is `is invalid`ated, we need to ensure that we don't
connect the analog value.

lib/Dialect/FIRRTL/Import/FIRParser.cpp
@@ -1636,10 +1636,11 @@ private:
 void FIRStmtParser::emitInvalidate(Value val, Flow flow) {
   auto tpe = val.getType().cast<FIRRTLType>();
 
-  if (tpe.isPassive()) {
-    if (flow == Flow::Source || tpe.isa<AnalogType>())
+  auto props = tpe.getRecursiveTypeProperties();
+  if (props.isPassive && !props.containsAnalog) {
+    if (flow == Flow::Source)
       return;
-    if (tpe.hasUninferredWidth())
+    if (props.hasUninferredWidth)
       builder.create<ConnectOp>(val, builder.create<InvalidValueOp>(tpe));
     else
       builder.create<StrictConnectOp>(val, builder.create<InvalidValueOp>(tpe));

test/Dialect/FIRRTL/parse-basic.fir
@@ -76,7 +76,8 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     input s1 : SInt<1>            ; CHECK: in %s1: !firrtl.sint<1>,
     input s8 : SInt<8>            ; CHECK: in %s8: !firrtl.sint<8>,
     input a1 : Analog<1>          ; CHECK: in %a1: !firrtl.analog<1>,
-    input a8 : Analog<8>          ; CHECK: in %a8: !firrtl.analog<8>)
+    input a8 : Analog<8>          ; CHECK: in %a8: !firrtl.analog<8>,
+    input ab : {x : Analog<1>}    ; CHECK: in %ab: !firrtl.bundle<x: analog<1>>)
 
     ; CHECK: %_t = firrtl.wire : !firrtl.vector<uint<1>, 12>
     wire _t : UInt<1>[12] @[Nodes.scala 370:76]
@@ -96,6 +97,9 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
 
     ; CHECK-NOT: firrtl.attach %a1
     a1 is invalid
+    
+    ; CHECK-NOT: firrtl.attach %ab
+    ab is invalid
 
     ; CHECK: firrtl.skip
     skip  @[SKipLoc.scala 42:24]