thomasnormal/circt

commit e08f5d528ca41a60f0dafff45192d7c1a4810297

[FIRRTL] Allow GC Companion multi-instantiation
Change the Grand Central View lowering pass to now allow for Grand Central
Companions to be multiply instantiated.  This should have technically been
possible for a long time [[1]] as long as there was a guarantee that there
was never going to be a leaf that was _outside_ the companion.

This is a pretty simple change to just loop over all instances and
extract (add a `lowerToBind` attribute) for the ones which are in the
design.  The other, subtle blocker is that without the `InstanceInfo`,
doing the check of "in the design" was tedious.  Now this is pretty
straightforward.

[1]: https://github.com/llvm/circt/commit/8b1ad061e0777704cfd4bb1c80196cb236307588

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Transforms/GrandCentral.cpp
@@ -2062,13 +2062,9 @@ void GrandCentralPass::runOnOperation() {
 
             // If this is a companion, then:
             //   1. Insert it into the companion map
-            //   2. Create a new mapping module.
-            //   3. Instantiate the mapping module in the companion.
-            //   4. Check that the companion is instantiated exactly once.
-            //   5. Set attributes on that lone instance so it will become a
-            //      bind if extraction information was provided.  If a DUT is
-            //      known, then anything in the test harness will not be
-            //      extracted.
+            //   2. Foreach instance of this companion, set attributes to
+            //      extract it if extraction information was provided and the
+            //      instance is inside the design.
             if (annotation.getClass() == companionAnnoClass) {
               builder.setInsertionPointToEnd(circuitOp.getBodyBlock());
 
@@ -2096,58 +2092,40 @@ void GrandCentralPass::runOnOperation() {
                 return true;
               }
 
-              // If the companion is instantiated above the DUT, then don't
-              // extract it.
-              if (!instanceInfo->allInstancesUnderEffectiveDut(op)) {
-                ++numAnnosRemoved;
-                return true;
-              }
+              // Extract all instances in the design.
+              for (auto *i : instancePaths->instanceGraph.lookup(op)->uses()) {
+                auto instance = cast<InstanceOp>(i->getInstance());
 
-              // Find the exact, one instance of this Grand Central companion.
-              InstanceOp instance;
-              for (auto *i :
-                   instancePathCache.instanceGraph.lookup(op)->uses()) {
-                if (!instance) {
-                  instance = cast<InstanceOp>(i->getInstance());
+                // Skip extraction if the instance is not in the design.
+                if (instance->getParentOfType<LayerBlockOp>())
                   continue;
+                if (!instanceInfo->allInstancesUnderEffectiveDut(
+                        i->getParent()->getModule()))
+                  continue;
+
+                if (companionMode == CompanionMode::Drop) {
+                  // Delete the instance if companions are disabled.
+                  OpBuilder builder(&getContext());
+                  for (auto port : instance->getResults()) {
+                    builder.setInsertionPointAfterValue(port);
+                    auto wire =
+                        builder.create<WireOp>(port.getLoc(), port.getType());
+                    port.replaceAllUsesWith(wire.getResult());
+                  }
+                  instance->erase();
+                } else {
+                  // Lower the companion to a bind unless the user told us
+                  // explicitly not to.
+                  if (companionMode == CompanionMode::Bind)
+                    instance->setAttr("lowerToBind", builder.getUnitAttr());
+
+                  instance->setAttr(
+                      "output_file",
+                      hw::OutputFileAttr::getFromFilename(
+                          &getContext(),
+                          maybeExtractInfo->bindFilename.getValue(),
+                          /*excludeFromFileList=*/true));
                 }
-
-                auto diag = op->emitOpError()
-                            << "is marked as a GrandCentral 'companion', but "
-                               "it is instantiated more than once";
-                for (auto *instance :
-                     instancePathCache.instanceGraph.lookup(op)->uses())
-                  diag.attachNote(instance->getInstance()->getLoc())
-                      << "it is instantiated here";
-                goto FModuleOp_error;
-              }
-              if (!instance) {
-                op->emitOpError() << "is marked as a GrandCentral 'companion', "
-                                     "but is never instantiated";
-                goto FModuleOp_error;
-              }
-
-              if (companionMode == CompanionMode::Drop) {
-                // Delete the instance if companions are disabled.
-                OpBuilder builder(&getContext());
-                for (auto port : instance->getResults()) {
-                  builder.setInsertionPointAfterValue(port);
-                  auto wire =
-                      builder.create<WireOp>(port.getLoc(), port.getType());
-                  port.replaceAllUsesWith(wire.getResult());
-                }
-                instance->erase();
-              } else {
-                // Lower the companion to a bind unless the user told us
-                // explicitly not to.
-                if (companionMode == CompanionMode::Bind)
-                  instance->setAttr("lowerToBind", builder.getUnitAttr());
-
-                instance->setAttr("output_file",
-                                  hw::OutputFileAttr::getFromFilename(
-                                      &getContext(),
-                                      maybeExtractInfo->bindFilename.getValue(),
-                                      /*excludeFromFileList=*/true));
               }
 
               // Look for any modules/extmodules _only_ instantiated by the

test/Dialect/FIRRTL/grand-central.mlir
@@ -1063,3 +1063,86 @@ firrtl.circuit "Top" attributes {
   // CHECK-NEXT:   sv.interface.signal @bar : i2
   // CHECK-NOT:    sv.interface @Bundle2_0
 }
+
+// -----
+
+firrtl.circuit "MultiplyInstantiated" attributes {
+  annotations = [
+    {class = "sifive.enterprise.grandcentral.AugmentedBundleType",
+     defName = "View",
+     elements = [
+       {class = "sifive.enterprise.grandcentral.AugmentedGroundType",
+        id = 42 : i64,
+        name = "field"}],
+     id = 0 : i64},
+    {class = "sifive.enterprise.grandcentral.ExtractGrandCentralAnnotation",
+     directory = "gct-dir",
+     filename = "gct-dir/bindings.sv"},
+    {class = "sifive.enterprise.grandcentral.GrandCentralHierarchyFileAnnotation",
+     filename = "gct.yaml"}]} {
+  firrtl.layer @A bind {}
+  firrtl.module private @View_companion() attributes {
+    annotations = [
+      {class = "sifive.enterprise.grandcentral.ViewAnnotation.companion",
+       defName = "Companion",
+       id = 0 : i64,
+       name = "View"}]} {
+    %0 = firrtl.constant 0 :!firrtl.uint<1>
+    %zero = firrtl.node  %0  {
+      annotations = [
+        {
+          class = "sifive.enterprise.grandcentral.AugmentedGroundType",
+          id = 42 : i64
+        }
+      ]
+    } : !firrtl.uint<1>
+  }
+  firrtl.module private @DUT() attributes {
+    annotations = [
+      {
+        class = "sifive.enterprise.firrtl.MarkDUTAnnotation"
+      }
+    ]
+  } {
+    firrtl.instance foo @View_companion()
+    firrtl.instance bar @View_companion()
+    firrtl.layerblock @A {
+      firrtl.instance baz @View_companion()
+    }
+  }
+  firrtl.module @MultiplyInstantiated() {
+    firrtl.instance dut @DUT()
+    firrtl.instance qux @View_companion()
+  }
+}
+
+// Only extract instances that are in the design.  The other instances should
+// not be extracted.
+//
+// CHECK:      sv.verbatim
+// CHECK-SAME:   - name: View
+// CHECK-SAME:     fields:
+// CHECK-SAME:       - name: field
+// CHECK-SAME:         dimensions: [ ]
+// CHECK-SAME:         width: 1
+// CHECK-SAME:     instances: []
+//
+// CHECK:      firrtl.module private @DUT()
+// CHECK-NEXT:   firrtl.instance foo
+// CHECK-SAME:     lowerToBind
+// CHECK-SAME:     output_file = #hw.output_file<"gct-dir{{/|\\\\}}bindings.sv"
+// CHECK-NEXT:   firrtl.instance bar
+// CHECK-SAME:     lowerToBind
+// CHECK-SAME:     output_file = #hw.output_file<"gct-dir{{/|\\\\}}bindings.sv"
+// CHECK-NEXT:   firrtl.layerblock @A {
+// CHECK-NEXT:     firrtl.instance baz
+// CHECK-NOT:       lowerToBind
+// CHECK-NOT:       output_file
+//
+// CHECK:      firrtl.module @MultiplyInstantiated()
+// CHECK-NEXT:   firrtl.instance dut @DUT()
+// CHECK-NEXT:   firrtl.instance qux
+// CHECK-NOT:      lowerToBind
+// CHECK-NOT:      output_file
+//
+// CHECK:      sv.interface

test/Dialect/FIRRTL/grand-central-errors.mlir
@@ -269,86 +269,6 @@ firrtl.circuit "InvalidField" attributes {
 
 // -----
 
-firrtl.circuit "MultiplyInstantiated" attributes {
-  annotations = [
-    {class = "sifive.enterprise.grandcentral.AugmentedBundleType",
-     defName = "Bar",
-     elements = [
-       {class = "sifive.enterprise.grandcentral.AugmentedGroundType",
-        id = 42 : i64,
-        name = "baz"}],
-     id = 0 : i64},
-    {class = "sifive.enterprise.grandcentral.ExtractGrandCentralAnnotation",
-     directory = "gct-dir",
-     filename = "gct-dir/bindings.sv"}]}  {
-  // expected-error @below {{'firrtl.module' op is marked as a GrandCentral 'companion', but it is instantiated more than once}}
-  firrtl.module private @View_companion() attributes {
-    annotations = [
-      {class = "sifive.enterprise.grandcentral.ViewAnnotation.companion",
-       defName = "Companion",
-       id = 0 : i64,
-       name = "View"}]} {
-    %0 = firrtl.constant 0 :!firrtl.uint<1>
-    %zero = firrtl.node  %0  {
-      annotations = [
-        {
-          class = "sifive.enterprise.grandcentral.AugmentedGroundType",
-          id = 42 : i64
-        }
-      ]
-    } : !firrtl.uint<1>
-  }
-  firrtl.module private @DUT() {
-    // expected-note @below {{it is instantiated here}}
-    firrtl.instance View_companion @View_companion()
-    // expected-note @below {{it is instantiated here}}
-    firrtl.instance View_companion @View_companion()
-  }
-  firrtl.module @MultiplyInstantiated() {
-    firrtl.instance dut @DUT()
-  }
-}
-
-// -----
-
-firrtl.circuit "NotInstantiated" attributes {
-  annotations = [
-    {class = "sifive.enterprise.grandcentral.AugmentedBundleType",
-     defName = "Bar",
-     elements = [
-       {class = "sifive.enterprise.grandcentral.AugmentedGroundType",
-        id = 42 : i64,
-        name = "baz"}],
-     id = 0 : i64},
-    {class = "sifive.enterprise.grandcentral.ExtractGrandCentralAnnotation",
-     directory = "gct-dir",
-     filename = "gct-dir/bindings.sv"}]}  {
-  // expected-error @below {{'firrtl.module' op is marked as a GrandCentral 'companion', but is never instantiated}}
-  firrtl.module private @View_companion() attributes {
-    annotations = [
-      {class = "sifive.enterprise.grandcentral.ViewAnnotation.companion",
-       defName = "Companion",
-       id = 0 : i64,
-       name = "View"}]} {
-    %0 = firrtl.constant 0 :!firrtl.uint<1>
-    %zero = firrtl.node  %0  {
-      annotations = [
-        {
-          class = "sifive.enterprise.grandcentral.AugmentedGroundType",
-          id = 42 : i64
-        }
-      ]
-    } : !firrtl.uint<1>
-  }
-  firrtl.module private @DUT() {
-  }
-  firrtl.module @NotInstantiated() {
-    firrtl.instance dut @DUT()
-  }
-}
-
-// -----
-
 firrtl.circuit "CompanionWithOutputs"
   attributes {annotations = [
     {class = "sifive.enterprise.grandcentral.AugmentedBundleType",

commit a6a1d0ee2db1615fc37c77c8cc69fbd89a13fd58

[FIRRTL] Fix Grand Central companion not-in-design (#8625)
Fix a regression introduced in e08f5d528 where a Grand Central companion
instantiated only under the test harness would cause the companion to be
instantiated in the Grand Central directory and not under the test harness
directory.  An early exit if no instances are in the design was
incorrectly dropped.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Transforms/GrandCentral.cpp
@@ -2131,6 +2131,13 @@ void GrandCentralPass::runOnOperation() {
                 }
               }
 
+              // If the companion is instantiated above the DUT, then don't
+              // extract it.
+              if (!instanceInfo->allInstancesUnderEffectiveDut(op)) {
+                ++numAnnosRemoved;
+                return true;
+              }
+
               // Look for any modules/extmodules _only_ instantiated by the
               // companion.  If these have no output file attribute, then mark
               // them as being extracted into the Grand Central directory.

test/Dialect/FIRRTL/grand-central.mlir
@@ -1146,3 +1146,56 @@ firrtl.circuit "MultiplyInstantiated" attributes {
 // CHECK-NOT:      output_file
 //
 // CHECK:      sv.interface
+
+// -----
+
+firrtl.circuit "CompanionInTestharness" attributes {
+  annotations = [
+    {class = "sifive.enterprise.grandcentral.AugmentedBundleType",
+     defName = "View",
+     elements = [
+       {class = "sifive.enterprise.grandcentral.AugmentedGroundType",
+        id = 42 : i64,
+        name = "field"}],
+     id = 0 : i64},
+    {class = "sifive.enterprise.grandcentral.ExtractGrandCentralAnnotation",
+     directory = "gct-dir",
+     filename = "gct-dir/bindings.sv"},
+    {class = "sifive.enterprise.grandcentral.GrandCentralHierarchyFileAnnotation",
+     filename = "gct.yaml"},
+    {class = "sifive.enterprise.firrtl.TestBenchDirAnnotation", dirname = "testbench"}]} {
+  firrtl.layer @A bind {}
+  firrtl.module private @View_companion() attributes {
+    annotations = [
+      {class = "sifive.enterprise.grandcentral.ViewAnnotation.companion",
+       defName = "Companion",
+       id = 0 : i64,
+       name = "View"}]} {
+    %0 = firrtl.constant 0 :!firrtl.uint<1>
+    %zero = firrtl.node  %0  {
+      annotations = [
+        {
+          class = "sifive.enterprise.grandcentral.AugmentedGroundType",
+          id = 42 : i64
+        }
+      ]
+    } : !firrtl.uint<1>
+  }
+  firrtl.module public @DUT() attributes {
+    annotations = [
+      {
+        class = "sifive.enterprise.firrtl.MarkDUTAnnotation"
+      }
+    ]
+  } {
+  }
+  firrtl.module @CompanionInTestharness() {
+    firrtl.instance dut @DUT()
+    firrtl.instance foo @View_companion()
+  }
+}
+
+// CHECK:     firrtl.module private @View_companion()
+// CHECK-NOT: output_file
+//
+// CHECK:     firrtl.module public @DUT()