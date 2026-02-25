9492753a41c85551e101d7c547b27f7df562fc36
Thomas Ahle <thomas@ahle.dk>
2026-02-25 18:01:48 +0000

    [wasm][tblgen] stabilize run-to-run callMain reentry

--- utils/wasm_tblgen_reentry_check.sh
+++ utils/wasm_tblgen_reentry_check.sh
@@ -27,6 +27,8 @@ fi
 tmpdir="$(mktemp -d)"
 trap 'rm -rf "$tmpdir"' EXIT
 out_td="$tmpdir/tblgen-reentry-out.td"
+out_td_run1="$tmpdir/tblgen-reentry-run1.td"
+out_td_run2="$tmpdir/tblgen-reentry-run2.td"
 
 echo "[wasm-tblgen-reentry] same-instance callMain: help -> print-records"
 set +e
@@ -43,6 +45,24 @@ set -e
 if [[ "$rc" -ne 0 ]]; then
   echo "[wasm-tblgen-reentry] re-entry run failed" >&2
   cat "$tmpdir/err.log" >&2
+  exit 1
+fi
+
+echo "[wasm-tblgen-reentry] same-instance callMain: print-records -> print-records"
+set +e
+"$NODE_BIN" "$REENTRY_HELPER" \
+  "$TBLGEN_JS" \
+  --first -print-records "$INPUT_TD" -I include -I llvm/include -I llvm/mlir/include -o "$out_td_run1" \
+  --second -print-records "$INPUT_TD" -I include -I llvm/include -I llvm/mlir/include -o "$out_td_run2" \
+  --expect-wasm-file-substr "$out_td_run1" "Classes" \
+  --expect-wasm-file-substr "$out_td_run2" "Classes" \
+  >"$tmpdir/runrun.out.log" 2>"$tmpdir/runrun.err.log"
+rc=$?
+set -e
+
+if [[ "$rc" -ne 0 ]]; then
+  echo "[wasm-tblgen-reentry] run->run re-entry failed" >&2
+  cat "$tmpdir/runrun.err.log" >&2
   exit 1
 fi
 
